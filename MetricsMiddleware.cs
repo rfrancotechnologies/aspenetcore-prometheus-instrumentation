ff1430c4b9627025ea038bd9dbf67aec5d9be60f
jsolana_cr <jsolana@rfranco.com>
2019-11-07 15:31:45 +0100

    If the http status code returned is not OK, then the error counter is increased.

--- MetricsMiddleware.cs
+++ MetricsMiddleware.cs
@@ -4,25 +4,23 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Prometheus;
 
-namespace Com.RFranco.Iris.AspNetCore.Prometheus
+namespace Com.RFranco.AspNetCore.Prometheus
 {
 
     public class MetricsMiddleware
     {
         private readonly RequestDelegate _next;
-        private static readonly Counter RequestsProcessed = Metrics.CreateCounter("total_successfull_requests", "Number of successfull processed requests.", "method");
-        private static readonly Counter ErrorRequestsProcessed = Metrics.CreateCounter("total_error_responses", "Number of unsuccessfull processed requests.", "method", "code_error");
-        private static readonly Gauge OngoingRequests = Metrics.CreateGauge("requests_in_progress", "Number of ongoing requests.", "method");
-        private static readonly Summary RequestsDurationSummaryInSeconds = Metrics.CreateSummary("requests_duration_summary_seconds", "A Summary of request duration (in seconds) over last 10 minutes.", "method");
-        private static readonly Histogram RequestResponseHistogram = Metrics.CreateHistogram("requests_duration_histogram_seconds", "Histogram of request duration in seconds.", "method");
+        private static readonly Counter RequestsProcessed = Metrics.CreateCounter("http_requests_total", "Number of successfull processed requests.", "method");
+        private static readonly Counter ErrorRequestsProcessed = Metrics.CreateCounter("http_error_total", "Number of unsuccessfull processed requests.", "method", "code_error");
+        private static readonly Gauge OngoingRequests = Metrics.CreateGauge("http_requests_in_progress", "Number of ongoing requests.", "method");
+        private static readonly Summary RequestsDurationSummaryInSeconds = Metrics.CreateSummary("http_requests_duration_summary_seconds", "A Summary of request duration (in seconds) over last 10 minutes.", "method");
+        private static readonly Histogram RequestResponseHistogram = Metrics.CreateHistogram("http_requests_duration_histogram_seconds", "Histogram of request duration in seconds.", "method");
 
-        
         public MetricsMiddleware(RequestDelegate next)
         {
             _next = next;
         }
 
-        
         public async Task Invoke(HttpContext context)
         {
             var fullMethodName = $"{context.Request.Method} {context.Request.Path.Value}";
@@ -43,6 +41,10 @@ namespace Com.RFranco.Iris.AspNetCore.Prometheus
                 finally
                 {
                     OngoingRequests.Labels(fullMethodName).Dec();
+                    
+                    if(context.Response.StatusCode != StatusCodes.Status200OK )
+                        ErrorRequestsProcessed.Labels(fullMethodName, context.Response.StatusCode.ToString()).Inc();
+                    
                     RequestsProcessed.Labels(fullMethodName).Inc();
                 }
             }
