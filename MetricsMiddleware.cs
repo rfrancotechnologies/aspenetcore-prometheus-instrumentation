ffd80919d585462c73f61aeb3518125922241e33
jsolana_cr <jsolana@rfranco.com>
2019-11-11 15:38:58 +0100

    Removed redundant metrics.

--- MetricsMiddleware.cs
+++ MetricsMiddleware.cs
@@ -16,10 +16,8 @@ namespace Com.RFranco.AspNetCore.Prometheus
         private const string UNKNOWN_REQUEST_PATH = "NotFound";
         private readonly RequestDelegate _next;
         private readonly IActionDescriptorCollectionProvider _actionDescriptorCollectionProvider;
-        private static readonly Counter RequestsProcessed = Metrics.CreateCounter("http_requests_total", "Number of successfull processed requests.", "method");
         private static readonly Counter ErrorRequestsProcessed = Metrics.CreateCounter("http_error_total", "Number of unsuccessfull processed requests.", "method", "code_error");
         private static readonly Gauge OngoingRequests = Metrics.CreateGauge("http_requests_in_progress", "Number of ongoing requests.", "method");
-        private static readonly Summary RequestsDurationSummaryInSeconds = Metrics.CreateSummary("http_requests_duration_summary_seconds", "A Summary of request duration (in seconds) over last 10 minutes.", "method");
         private static readonly Histogram RequestResponseHistogram = Metrics.CreateHistogram("http_requests_duration_histogram_seconds", "Histogram of request duration in seconds.", "method");
 
         public MetricsMiddleware(RequestDelegate next, IActionDescriptorCollectionProvider actionDescriptorCollectionProvider)
@@ -35,7 +33,6 @@ namespace Com.RFranco.AspNetCore.Prometheus
             OngoingRequests.Labels(fullMethodName).Inc();
 
             using (RequestResponseHistogram.Labels(fullMethodName).NewTimer())
-            using (RequestsDurationSummaryInSeconds.Labels(fullMethodName).NewTimer())
             {
                 try
                 {
@@ -53,7 +50,6 @@ namespace Com.RFranco.AspNetCore.Prometheus
                     if (context.Response.StatusCode != StatusCodes.Status200OK)
                         ErrorRequestsProcessed.Labels(fullMethodName, context.Response.StatusCode.ToString()).Inc();
 
-                    RequestsProcessed.Labels(fullMethodName).Inc();
                 }
             }
         }
